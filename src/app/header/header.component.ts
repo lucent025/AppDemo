45317332d4ac6807a18404157299150b490b1e45
lucent025 <smile25003@gmail.com>
2025-03-12 17:41:24 +0700

    search query feat

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  products: any[] = [];
+  constructor(private dataService: DataService) {}
+
   totalProducts: number = 0;
 
   menuVisible: boolean = false;
+
+  ngOnInit() {
+    const storedCartItems = localStorage.getItem('cart');
+    this.totalProducts = JSON.parse(storedCartItems || '[]').length;
+    this.dataService.getProduct().subscribe(data => {
+      this.products = data; // Lưu dữ liệu JSON vào biến items
+    });
+  }
 
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
@@ -18,16 +30,34 @@ export class HeaderComponent {
     }
   }
 
-  ngOnInit() {
-    const storedCartItems = localStorage.getItem('cart');
-    this.totalProducts = JSON.parse(storedCartItems || '[]').length;
-  }
-
   ngDoCheck() {
     const storedCartItems = localStorage.getItem('cart');
     const currentCartLength = JSON.parse(storedCartItems || '[]').length;
     if (this.totalProducts !== currentCartLength) {
       this.totalProducts = currentCartLength;
     }
+  }
+
+  searchResults: any[] = [];
+  isResultsVisible: boolean = false;
+
+  onSearch(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const query = input.value.toLowerCase();
+    // const allProducts = ['Product 1', 'Product 2', 'Product 3']; // Example product list
+
+    this.searchResults = this.products.filter(prod => prod.name.toLowerCase().charAt(0) === query.charAt(0)); // Lọc kết quả theo query
+    this.isResultsVisible = this.searchResults.length > 0 && query.length > 0;
+    // console.log(this.searchResults);
+  }
+
+  hideResults() {
+    setTimeout(() => {
+      this.isResultsVisible = false;
+    }, 200); // Tránh mất focus ngay lập tức
+  }
+
+  showResults() {
+    this.isResultsVisible = true;
   }
 }
